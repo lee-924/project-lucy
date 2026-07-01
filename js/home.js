720db4238cd3395c50a32b2dcc5d15e7e9ce3840
lee <ahumada14344@gmail.com>
2026-07-01 15:46:12 +0300

    Fix case of URL in b2 click event

--- js/home.js
+++ js/home.js
@@ -11,5 +11,5 @@ b1.addEventListener('click', () => {
 
 let b2 = document.querySelector('.b2')
 b2.addEventListener('click', () => {
-    window.location.href = 'surprises.html'
+    window.location.href = 'Surprises.html'
 })
